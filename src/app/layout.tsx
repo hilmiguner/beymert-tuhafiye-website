a21577579ec3da83b9f576d8e128a55dd74bdfad
Hilmi Güner <94684527+hilmiguner@users.noreply.github.com>
2026-09-20 14:04:51 +0300

    fix: declare smooth scroll behavior to Next.js

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -34,7 +34,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="tr">
+    <html lang="tr" data-scroll-behavior="smooth">
       <body>
         <a href="#main-content" className="bt-skip-link">
           Ana içeriğe geç
